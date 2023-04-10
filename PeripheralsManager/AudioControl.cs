c419bf25c13e6e01474b47e0d6d39f9683c45b6a
encyk <martiiin225@email.cz>
2023-04-10 16:00:34 +0200

    feat: some testing of audio

---
+++ PeripheralsManager/AudioControl.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PeripheralsManager;
+
+internal class AudioControl
+{
+    //bruh, nejspis budu muset pouzit tu knihovnu
+    // https://stackoverflow.com/a/75578979/12741390 (tohle je na nalezeni devices, umi to i audio control)
+    // NAudio
+
+    // tak me napada
+    // lepsi asi bude odkazovat na takove to divne id tech zarizeni, ne jejich jmeno. Takove to s dvema parama {0000-0-0-0--000}.{alksdjflaksjflkasjdlfkj}
+
+
+
+
+    #region VolumeUp/Down, method with SendMessage()
+    // source https://stackoverflow.com/a/57373074/12741390
+    [DllImport("user32.dll")]
+    static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+    const int WM_APPCOMMAND = 0x319;
+    const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+    const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+    const int APPCOMMAND_VOLUME_UP = 0xA0000;
+    readonly IntPtr _handle;
+
+    public AudioControl(IntPtr handle)
+    {
+        _handle = handle;
+    }
+
+    public void VolumeUp_Messages()
+    //TODO: not ideal, only works in increments of 2???!
+    {
+        SendMessage(_handle, WM_APPCOMMAND, IntPtr.Zero, (IntPtr)APPCOMMAND_VOLUME_UP);
+    }
+
+    public void VolumeDown_Messages()
+    {
+        SendMessage(_handle, WM_APPCOMMAND, IntPtr.Zero, (IntPtr)APPCOMMAND_VOLUME_DOWN);
+    }
+
+    public void ToggleMute_Messages()
+    {
+        SendMessage(_handle, WM_APPCOMMAND, IntPtr.Zero, (IntPtr)APPCOMMAND_VOLUME_MUTE);
+    }
+    #endregion
+
+    #region detecting audio devices
+    //TODO: decide and move to a separate file from this one 
+    [DllImport("winmm.dll", SetLastError = true)] static extern uint waveOutGetNumDevs();
+    [DllImport("winmm.dll", SetLastError = true, CharSet = CharSet.Auto)] public static extern uint waveOutGetDevCaps(uint hwo, ref WAVEOUTCAPS pwoc, uint cbwoc);
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public struct WAVEOUTCAPS
+    {
+        public ushort wMid;
+        public ushort wPid;
+        public uint vDriverVersion;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)] public string szPname; //TODO: ziskat cela jmena z registruy podle https://stackoverflow.com/a/17467009/12741390 //note: WAVEOUTCAPS pry vraci guid, podle toho by to slo jednoduseji hledat, nez podle jmena
+        public uint dwFormats;
+        public ushort wChannels;
+        public ushort wReserved1;
+        public uint dwSupport;
+    }
+
+    public static string[] GetSoundDevices()
+    {
+        uint devices = waveOutGetNumDevs();
+        string[] result = new string[devices];
+        WAVEOUTCAPS caps = new WAVEOUTCAPS();
+
+        for (uint i = 0; i < devices; i++)
+        {
+            waveOutGetDevCaps(i, ref caps, (uint)Marshal.SizeOf(caps));
+            result[i] = caps.szPname;
+        }
+        return result;
+    }
+
+    // ----
+    [DllImport("dsound.dll", CharSet = CharSet.Ansi)] static extern void DirectSoundCaptureEnumerate(DSEnumCallback callback, IntPtr context);
+
+    delegate bool DSEnumCallback([MarshalAs(UnmanagedType.LPStruct)] Guid guid, string description, string module, IntPtr lpContext);
+
+    static bool EnumCallback(Guid guid, string description, string module, IntPtr context)
+    {
+        if (guid != Guid.Empty)
+            captureDevices.Add(new DirectSoundDeviceInfo(guid, description, module));
+        return true;
+    }
+    private static List<DirectSoundDeviceInfo> captureDevices;
+
+    public static IEnumerable<DirectSoundDeviceInfo> GetCaptureDevices()
+    {
+        captureDevices = new List<DirectSoundDeviceInfo>();
+        DirectSoundCaptureEnumerate(new DSEnumCallback(EnumCallback), IntPtr.Zero);
+        return captureDevices;
+    }
+
+    public class DirectSoundDeviceInfo
+    {
+        public DirectSoundDeviceInfo(Guid guid, string description, string module)
+        { Guid = guid; Description = description; Module = module; }
+        public Guid Guid { get; }
+        public string Description { get; }
+        public string Module { get; }
+    }
+    #endregion
+
+
+}
--- PeripheralsManager/PresetForm.cs
+++ PeripheralsManager/PresetForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace PeripheralsManager;
 
@@ -15,6 +16,7 @@ public partial class PresetForm : Form
     public string ProfileName { get; private set; } = "";
     public int MouseSpeed { get; private set; } = 10;
     //TODO: check this for mouse speed: https://liquipedia.net/counterstrike/Mouse_Settings#Windows_Sensitivity
+    private AudioControl _audioControl;
 
     public PresetForm()
     {
@@ -23,23 +25,58 @@ public partial class PresetForm : Form
 
         // find the devices
         //bruh
+        string[] audioDevices = AudioControl.GetSoundDevices();
+        foreach (string device in audioDevices)
+        {
+            AudioDevices_ListBox.Items.Add(device);
+        }
 
+        //var captureDevices = AudioControl.GetCaptureDevices();
+        //foreach(AudioControl.DirectSoundDeviceInfo device in captureDevices)
+        //{
+        //    AudioDevices_ListBox.Items.Add(device.Description);
+        //    MessageBox.Show($"{device.Guid}, {device.Module}, {device.Description}");
+        //}
+        //TODO: maybe I should add microphones too? hmmm...
+
+        // maybe this way, huge block of code, but uses registry instead of external libs https://stackoverflow.com/a/50518590/12741390
+
+        // Get current mouse sensitivity
+        try
+        {
+            using (RegistryKey? key = Registry.CurrentUser.OpenSubKey("Control Panel\\Mouse")) //TODO: might have to save this to use it in the future for saving sens, idk
+            {
+                if (key != null)
+                {
+                    object? o = key.GetValue("MouseSensitivity");
+                    if (o != null)
+                    {
+                        MouseSensitivity_TrackBar.Value = Convert.ToInt32(o.ToString());
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            throw; //TODO: ngl not sure what to do here
+        }
+
+        // Get current volume
+        _audioControl = new AudioControl(this.Handle);
+
+        // ?mby https://superuser.com/questions/1644356/how-to-get-system-volume-levelaudio-in-windows
+        // → http://www.nirsoft.net/utils/sound_volume_view.html#command_line - spawnout proces teto aplikace a tahat z toho data
+
+        // mozna https://blog.sverrirs.com/2016/02/windows-coreaudio-api-in-c.html ?
+
+        //idek, tohle je nejake cpp https://learn.microsoft.com/en-us/windows/win32/coreaudio/audio-endpoint-devices
     }
 
-    private void Mouse_CB_CheckedChanged(object sender, EventArgs e)
-    {
-        Mouse_Group.Enabled = Mouse_CB.Checked;
-    }
+    private void Mouse_CB_CheckedChanged(object sender, EventArgs e) => Mouse_Group.Enabled = Mouse_CB.Checked;
 
-    private void Audio_CB_CheckedChanged(object sender, EventArgs e)
-    {
-        Audio_Group.Enabled = Audio_CB.Checked;
-    }
+    private void Audio_CB_CheckedChanged(object sender, EventArgs e) => Audio_Group.Enabled = Audio_CB.Checked;
 
-    private void Name_TB_TextChanged(object sender, EventArgs e)
-    {
-        ProfileName = Name_TB.Text;
-    }
+    private void Name_TB_TextChanged(object sender, EventArgs e) => ProfileName = Name_TB.Text;
 
     private void Save_Btn_Click(object sender, EventArgs e)
     {
@@ -69,5 +106,20 @@ public partial class PresetForm : Form
     {
         if (Volume_TrackBar.Value != Volume_UpDown.Value)
             Volume_TrackBar.Value = (int)Volume_UpDown.Value;
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        _audioControl.VolumeDown_Messages();
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+        _audioControl.VolumeUp_Messages();
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+       _audioControl.ToggleMute_Messages();
     }
 }
